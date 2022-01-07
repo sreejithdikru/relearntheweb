74140ef66cb729cb4405406bea7f959a73989a9e
sreejithdikru <imsreejithofficial@gmail.com>
2022-01-07 11:30:52 +0530

    restructure

--- trial1/scripts/main.js
+++ trial1/scripts/main.js
@@ -14,4 +14,17 @@ myImage.onclick = function() {
       myImage.setAttribute('src','images/vijayimg1.png');
     }
 }
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
